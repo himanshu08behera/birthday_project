0db3bc2bfd5ba8f95c08379556d0d01a319bb8b7
HIMANSHU SEKHAR BEHERA <himanshubehera1129@gmail.com>
2026-06-02 00:36:56 +0530

    Update settings.py

--- birthday_project/settings.py
+++ birthday_project/settings.py
@@ -47,18 +47,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
 ]
 
 ROOT_URLCONF = 'birthday_project.urls'
